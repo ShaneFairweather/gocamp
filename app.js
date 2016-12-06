dd27b89d153837153f1559c5b37549d95982ee2e
smafair <shane.fairweather@yahoo.com>
2016-12-06 22:38:37 +0000

    add mongo lab

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var campgroundRoutes = require('./routes/campgrounds');
 var authRoutes = require('./routes/index');
 
 
-mongoose.connect('mongodb://localhost/yelp_camp');
+//mongoose.connect('mongodb://localhost/yelp_camp');
+mongoose.connect('mongodb://smafair:stormof59@ds127958.mlab.com:27958/gocamp');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 app.set('view engine', 'ejs');
